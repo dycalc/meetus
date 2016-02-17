4428acbbaf7e8cf50932b3fbf8751a996ff1a378
dycalc <dycalc@163.com>
2016-02-18 00:46:47 +0800

    rails10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -17,6 +16,9 @@ Rails.application.routes.draw do
   # post '/issues' => 'issues#create'
   # get 'issues/:id/edit' => 'issues#edit', :as => 'edit_issue'
   # patch 'issues/:id' => 'issues#update'
+
+  # comments
+  post '/issues/:issue_id/comments' => 'comments#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110062712) do
+ActiveRecord::Schema.define(version: 20160217155453) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content",    limit: 65535
+    t.string   "username",   limit: 255
+    t.string   "email",      limit: 255
+    t.integer  "issue_id",   limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "issues", force: :cascade do |t|
     t.string   "title",      limit: 255
